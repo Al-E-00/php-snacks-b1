17b0db6b0f8f170407636da22ccf4457a932a86f
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-08 16:25:55 +0200

    get data from input works

--- snack2.php
+++ snack2.php
@@ -7,9 +7,9 @@ e che age sia un numero. Se tutto è ok stampare “Accesso
 riuscito”, altrimenti “Accesso negato” -->
 
 <?php
-$name = key_exists('name', $_GET) ? $_GET('name') : '';
-$mail = key_exists('mail', $_GET) ? $_GET('mail') : '';
-$age = key_exists('age', $_GET) ? $_GET('age') : '';
+$name = key_exists('name', $_GET) ? $_GET['name'] : ' ';
+$mail = key_exists('mail', $_GET) ? $_GET['mail'] : ' ';
+$age = key_exists('age', $_GET) ? $_GET['age'] : ' ';
 ?>
 
 <!DOCTYPE html>
@@ -29,16 +29,16 @@ $age = key_exists('age', $_GET) ? $_GET('age') : '';
         <form>
             <div class="mb-3">
                 <label for="nameInput" class="form-label">Name</label>
-                <input type="text" class="form-control" id="nameInput" value='<?php echo $name ?>' />
+                <input type="text" class="form-control" id="nameInput" name='name' value='<?php echo $name ?>' />
             </div>
             <div class="mb-3">
                 <label for="mailInput" class="form-label">Mail</label>
-                <input type="mail" class="form-control" id="mailInput" aria-describedby="emailHelp" value='<?php echo $mail ?>' />
+                <input type="mail" class="form-control" id="mailInput" name='mail' aria-describedby="emailHelp" value='<?php echo $mail ?>' />
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="ageInput" class="form-label">Age</label>
-                <input type="number" class="form-control" id="ageInput" value='<?php echo $age ?>' />
+                <input type="number" class="form-control" id="ageInput" name='age' value='<?php echo $age ?>' />
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
