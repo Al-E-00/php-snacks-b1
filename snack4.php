26c06520d82de10c522ccac5a10a0aad0d4ce365
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-08 18:36:26 +0200

    snack 3 completed

--- snack4.php
+++ snack4.php
@@ -4,6 +4,15 @@ $numberArray = [];
 
 $i = 0;
 
-let ($i; $i < 15; $i++) {
-    array_push($numberArray, rand(0, 100))
+for ($i; $i < 15; $i++) {
+    $casualNum = rand(0, 15);
+    if (in_array($casualNum, $numberArray) == false) {
+        array_push($numberArray, $casualNum);
+    } else {
+        $i--;
+    }
 }
+
+var_dump($numberArray);
+
+?>
